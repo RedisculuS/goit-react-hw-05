fb3b3f6e3c121e2dfe96f2daa5191768a8944c91
RedisculuS <47760888+RedisculuS@users.noreply.github.com>
2024-08-21 12:56:34 +0300

    5th commit
    fixed "go back" btn
    added rule to file .eslintrc.cjs

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import css from './MovieList.module.css';
 
@@ -7,10 +7,14 @@ export default function MovieList({ movies }) {
 
   return (
     <ul className={css.container}>
-      {movies.map((movie) => (
-        <li key={movie.id}  className={css.cardWrapper}>
-          <Link to={`/movies/${movie.id}`} state={{ from: location.pathname }}>
-            <img className={css.posterFormatImg} src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.original_title} />
+      {movies.map(movie => (
+        <li key={movie.id} className={css.cardWrapper}>
+          <Link to={`/movies/${movie.id}`} state={{ from: location }}>
+            <img
+              className={css.posterFormatImg}
+              src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+              alt={movie.original_title}
+            />
             <h3 className={css.movieName}>{movie.original_title}</h3>
           </Link>
         </li>
